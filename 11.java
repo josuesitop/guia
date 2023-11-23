65e478a7a88d3a787ca242e8d42f23f88c5e2f2f
josuesitop <143621811+josuesitop@users.noreply.github.com>
2023-11-23 16:40:18 -0600

    Add files via upload

---
+++ 11.java
@@ -0,0 +1,16 @@
+public class datosYejemplos {
+    public static void main(String[] args) {
+        
+        byte byteVariable = 127;
+        short shortVariable = 2023;
+        int intVariable = 7;
+        long longVariable = 111111111L; 
+        float floatVariable = 10.0f; 
+        double doubleVariable = 3.1416;
+        char charVariable = 'z';
+        boolean booleanVariable = false;
+        String stringVariable = "guia de programacion";
+
+        
+    }
+}
